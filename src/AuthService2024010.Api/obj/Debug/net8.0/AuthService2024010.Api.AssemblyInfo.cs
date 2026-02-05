e3b2b5c71a6d3a86c983763c84cb6d1722dbe5c9
alacan-2024010 <alacan-2024010@kinal.edu.gt>
2026-02-05 16:12:48 -0600

    Se corrigieron errores en la sintaxis

--- src/AuthService2024010.Api/obj/Debug/net8.0/AuthService2024010.Api.AssemblyInfo.cs
+++ src/AuthService2024010.Api/obj/Debug/net8.0/AuthService2024010.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService2024010.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d41afe8a5bf7bd93ebea450f4a76657a379ba4a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c8ed7c62ad449054401575c04b32b7d3859dff4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService2024010.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService2024010.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/AuthService2024010.Persistence/obj/Debug/net8.0/AuthService2024010.Persistence.AssemblyInfo.cs
+++ src/AuthService2024010.Persistence/obj/Debug/net8.0/AuthService2024010.Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService2024010.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a85cca5d4a3ff38e39801f2e713c06ac986f9f00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c8ed7c62ad449054401575c04b32b7d3859dff4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService2024010.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService2024010.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
